20996ba9b3cde100c984941b1214ac57e17ddcf6
gean <geansantos0808@gmail.com>
2022-09-28 14:08:32 -0300

    settings

--- src/components/SelectDoctor/Doctor.tsx
+++ src/components/SelectDoctor/Doctor.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { DoctorStyled } from './styles';
 import { database } from '../../example/specialtyData';

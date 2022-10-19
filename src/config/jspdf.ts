3326e818fd3bf0745a979877a9e2f4c7a9d8ff8b
gean <geansantos0808@gmail.com>
2022-10-19 09:04:47 -0300

    editing jspf

--- src/config/jspdf.ts
+++ src/config/jspdf.ts
@@ -14,6 +14,8 @@ export default function jspdf(props: Props) {
   // eslint-disable-next-line new-cap
   const doc = new jsPDF();
 
+  console.log(props);
+
   doc.text('./teste.html', 10, 10);
   doc.output('dataurlnewwindow');
 }
